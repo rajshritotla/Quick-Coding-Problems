be908627857f0771e0b4645f341b6665be976cac
Rajshri Totla <rajshreetotla@gmail.com>
2019-06-29 20:05:31 -0400

    Determine if a string has all unique characters

---
+++ IsUnique.java
@@ -0,0 +1,36 @@
+// determine if a string has all unique characters
+
+import java.util.HashSet;
+
+public class IsUnique{
+
+    public boolean checkUnique(String str){
+        HashSet<String> characters = new HashSet<String>();
+        for(int i=0; i<str.length() && i<128; i++)          // considering 128 char
+        {
+            // if char exists
+            if(characters.contains(str.charAt(i)+"")) return false;
+            // else add char to set
+            else characters.add(str.charAt(i)+"");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        String str = "cat";
+        System.out.println(str+" isUnique "+new IsUnique().checkUnique(str));
+        str = "hello";
+        System.out.println(str+" isUnique "+new IsUnique().checkUnique(str));
+
+    }
+
+}
+
+/*
+
+OUTPUT
+cat isUnique true
+hello isUnique false
+
+ */
