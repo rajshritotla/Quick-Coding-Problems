9f9df41be8f1ead2b2003ce9e82da776b77f1dd5
Rajshri Totla <rajshreetotla@gmail.com>
2019-09-12 20:52:08 -0400

    LRU cache 1 implemt

---
+++ LRUCache.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+
+import com.sun.org.apache.xerces.internal.parsers.IntegratedParserConfiguration;
+
+class LRUCache {
+
+    private int cap;
+    Deque<Integer> dq;
+    HashMap<Integer,Integer> map;
+    
+    public LRUCache(int capacity) {    
+        cap = capacity;
+        dq  = new LinkedList<Integer>();
+        map = new HashMap<Integer,Integer>();
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key))
+            return map.get(key);
+        else
+            return -1;
+    }
+    
+    public void put(int key, int value) {
+       
+        
+           if(!map.containsKey(key)) {
+                if(dq.size()==cap){
+                   int last = dq.pollLast();
+                   map.remove(last);
+               }        
+           }else{
+               int index =0, i=0;
+               Iterator<Integer> iq = dq.iterator();
+               while(iq.hasNext()){
+                   if(iq.next()==key){
+                        index=i;
+                        break;
+                   }
+                   i++;
+               }
+               dq.remove(index);
+           }
+        dq.add(key);
+        map.put(key,value);        
+    }
+
+
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
