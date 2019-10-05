0563ca1a6b6a32380fa79d885460790ac22c816e
Rajshri Totla <rajshreetotla@gmail.com>
2019-10-05 12:47:34 -0400

    reverse string and a sentence

---
+++ ReverseString.java
@@ -0,0 +1,27 @@
+public class ReverseString{
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseString().reverseWord("abc"));
+        System.out.println(new ReverseString().reverseSentence("Wow! It's done!"));
+    }
+
+    String reverseWord(String s) {
+        StringBuffer str = new StringBuffer(s);
+        return s.reverse().toString();
+    }
+
+    String reverseSentence(String s){
+        String[] str = s.split(" ");
+        String sentence = "";
+        for(int i=0; i<str.length; i++){
+            if(i!=0) {
+                sentence = sentence + " " + reverseWord(str[i]);
+            } else {
+                sentence = sentence + reverseWord(str[i]);
+            }
+        }
+        return sentence;
+    }
+
+
+}
