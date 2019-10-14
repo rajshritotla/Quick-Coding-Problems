57b30e94bd9a8f1fb7cbe0207aefd8da03b3d259
Rajshri Totla <rajshreetotla@gmail.com>
2019-10-14 15:42:03 -0400

    Create PrimePalindrome.java

---
+++ PrimePalindrome.java
@@ -0,0 +1,39 @@
+/*
+Find the smallest prime palindrome greater than or equal to N.
+
+Recall that a number is prime if it's only divisors are 1 and itself, and it is greater than 1. 
+
+For example, 2,3,5,7,11 and 13 are primes.
+
+Recall that a number is a palindrome if it reads the same from left to right as it does from right to left. 
+
+For example, 12321 is a palindrome.
+*/
+class Solution {
+    public int primePalindrome(int N) {
+        while (true) {
+            if (N == reverse(N) && isPrime(N))
+                return N;
+            N++;
+            if (10_000_000 < N && N < 100_000_000)
+                N = 100_000_000;
+        }
+    }
+
+    public boolean isPrime(int N) {
+        if (N < 2) return false;
+        int R = (int) Math.sqrt(N);
+        for (int d = 2; d <= R; ++d)
+            if (N % d == 0) return false;
+        return true;
+    }
+
+    public int reverse(int N) {
+        int ans = 0;
+        while (N > 0) {
+            ans = 10 * ans + (N % 10);
+            N /= 10;
+        }
+        return ans;
+    }
+}
